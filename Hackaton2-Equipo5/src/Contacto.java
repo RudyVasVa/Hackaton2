9855e7c0069495b88d2f44e2f217f222a3f3f228
Lizvega1993 <lizethvegagomez@hotmail.com>
2024-02-08 14:20:06 -0500

    Creacion de la clase Contacto

--- Hackaton2-Equipo5/src/Contacto.java
+++ Hackaton2-Equipo5/src/Contacto.java
@@ -1,2 +1,36 @@
 public class Contacto {
+
+    private String nombre;
+    private int telefono;
+
+
+    public Contacto(String nombre, int telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * Metodo para mostrar informacion del contacto
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "{ Nombre: " + this.nombre + " , " + "Telefono: " + this.telefono + " }" ;
+    }
 }
